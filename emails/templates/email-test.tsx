86e6221bd7b3eba51ab1afc45a1d640104a41974
Hendrik Saly <hendrik.saly@eliatra.com>
2024-12-25 23:53:14 +0100

    Add email support

---
+++ emails/templates/email-test.tsx
@@ -0,0 +1,45 @@
+import { render, Text, Img, Container } from "jsx-email";
+import { EmailLayout } from "../layout";
+import {
+  createVariablesHelper,
+  GetSubject,
+  GetTemplate,
+  GetTemplateProps,
+} from "keycloakify-emails";
+
+interface TemplateProps extends Omit<GetTemplateProps, "plainText"> {}
+
+const paragraph = {
+  color: "#777",
+  fontSize: "16px",
+  lineHeight: "24px",
+  textAlign: "left" as const,
+};
+
+export const previewProps: TemplateProps = {
+  locale: "en",
+  themeName: "vanilla",
+};
+
+export const templateName = "Email Test";
+
+const { exp } = createVariablesHelper("email-test.ftl");
+
+const baseUrl = import.meta.isJsxEmailPreview ? "/assets" : "${url.resourcesUrl}";
+
+export const Template = ({ locale }: TemplateProps) => (
+  <EmailLayout preview={"Here is a preview"} locale={locale}>
+    <Container>
+      <Img src={`${baseUrl}/kc-logo.png`} alt="KC Logo" width="83" height="75" />
+    </Container>
+    <Text style={paragraph}>This is a test message from {exp("realmName")}</Text>
+  </EmailLayout>
+);
+
+export const getTemplate: GetTemplate = async (props) => {
+  return await render(<Template {...props} />, { plainText: props.plainText });
+};
+
+export const getSubject: GetSubject = async (_props) => {
+  return "[KEYCLOAK] - SMTP test message";
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
+import {buildEmailTheme} from "keycloakify-emails";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         keycloakify({
-            accountThemeImplementation: "none"
+            accountThemeImplementation: "none",
+            postBuild: async (buildContext) => {
+                await buildEmailTheme({
+                    templatesSrcDirPath: import.meta.dirname + "/emails/templates",
+                    themeNames: buildContext.themeNames,
+                    keycloakifyBuildDirPath: buildContext.keycloakifyBuildDirPath,
+                    locales: ["en", "pl"],
+                    cwd: import.meta.dirname,
+                    esbuild: {}, // optional esbuild options
+                });
+            },
         })
     ]
 });
